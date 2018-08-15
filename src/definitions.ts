68e2967c60561c46cb4f3424352a1c4230bfd473
Sean Bannigan <bannigan@meetmaestro.com>
2018-08-14 21:47:51 -0400

    refactor iOS plugin

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,37 +1,59 @@
+import { PluginListenerHandle } from '@capacitor/core';
+
 declare global {
-  interface PluginRegistry {
-    CapacitorVideoRecorderPlugin?: ICapacitorVideoRecorder;
-  }
+	interface PluginRegistry {
+		VideoRecorder?: VideoRecorder;
+	}
 }
 
-export interface ICapacitorVideoRecorder {
-  initialize(): Promise<any>;
-  destroy(): Promise<any>;
-  toggleCamera(): Promise<any>;
-  show(options: CapacitorVideoRecorderOptions): Promise<any>;
-  hide(): Promise<any>;
-  togglePip(): Promise<{ pictureInPicture: boolean }>;
-  startRecording(): Promise<any>;
-  stopRecording(): Promise<{ videoUrl: string }>;
-  getDuration(): Promise<{ value: number }>;
+export interface VideoRecorder {
+	initialize(options?: VideoRecorderOptions): Promise<void>;
+	destroy(): Promise<void>;
+	flipCamera(): Promise<void>;
+	addPreviewFrameConfig(config: VideoRecorderPreviewFrame): Promise<void>;
+	editPreviewFrameConfig(config: VideoRecorderPreviewFrame): Promise<void>;
+	switchToPreviewFrame(options: { id: string }): Promise<void>;
+	showPreviewFrame(): Promise<void>;
+	hidePreviewFrame(): Promise<void>;
+	startRecording(): Promise<void>;
+	stopRecording(): Promise<{ videoUrl: string }>;
+	getDuration(): Promise<{ value: number }>;
+	addListener(eventName: 'onDurationChange' | 'onVolumeInput', listenerFunc: (event: {value: number}) => void): PluginListenerHandle;
 }
 
-export interface CapacitorVideoRecorderOptions {
-  position?: CapacitorVideoRecorderPosition;
-  quality?: CapacitorVideoRecorderQuality;
+export interface VideoRecorderPreviewFrame {
+	id: string;
+	stackPosition?: 'front' | 'back';
+	x?: number;
+	y?: number;
+	width?: number | 'fill';
+	height?: number | 'fill';
+	borderRadius?: number;
+	dropShadow?: {
+		opacity?: number;
+		radius?: number;
+		color?: string;
+	}
 }
 
-export enum CapacitorVideoRecorderPosition {
-  FRONT,
-  BACK
+export interface VideoRecorderOptions {
+	camera?: VideoRecorderCamera;
+	quality?: VideoRecorderQuality;
+	autoShow?: boolean;
+	previewFrames?: VideoRecorderPreviewFrame[];
 }
 
-export enum CapacitorVideoRecorderQuality {
-  MAX_480P,
-  MAX_720P,
-  MAX_1080P,
-  MAX_2160P,
-  HIGHEST,
-  LOWEST,
-  QVGA
+export enum VideoRecorderCamera {
+	FRONT,
+	BACK
+}
+
+export enum VideoRecorderQuality {
+	MAX_480P,
+	MAX_720P,
+	MAX_1080P,
+	MAX_2160P,
+	HIGHEST,
+	LOWEST,
+	QVGA
 }
